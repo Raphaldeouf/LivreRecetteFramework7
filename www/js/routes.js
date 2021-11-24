3055d13e858684e2e63ad383370f5c541ec76797
Raphaldeouf <raphaelvannier2001@gmail.com>
2021-11-24 16:34:36 +0100

    debug

--- www/js/routes.js
+++ www/js/routes.js
@@ -2,69 +2,92 @@
 var routes = [
   {
     path: '/',
-    url: './index.html',
+    url: '/index.html'
   },
   {
     path: '/about/',
     url: './pages/about.html',
   },
+
+  {
+    path: '/home/',
+    componentUrl: './pages/home.html',
+  },
+  {
+    path: '/recette/:recetteId/',
+    url: "./pages/recette.html",
+    on: {
+      pageAfterIn: function(e, page){
+        var router = this;
+        var app = router.app;
+        var recipeId = page.route.params.recetteId;
+        console.log(recipeId);
+        app.request({
+          url: "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + recipeId, //URL de L'api
+          method: "GET", // Méthode 
+          dataType: "json", // Important, sinon vous allez récupérer un string et non un objet
+          beforeSend: function () {
+            // Avant de récupérer mes datas, j'affiche un loader 
+            //(important quand on fait un traitement pour montrer qu'il est en cours +  empêcher les impatients de cliquer partout pendant le process !)
+            app.dialog.preloader();
+          },
+          success: function (res) {
+            // res correspond à la réponse
+            console.log(res);
+            $('.recipe-name').html(res.meals[0].strMeal)
+            $('.recipe-thumbnail').html(res.meals[0].strMealThumb)
+            $('.recipe-instructions').html(res.meals[0].strInstructions)
+
+            // Je ferme le loader
+            app.dialog.close();
+          },
+        });
+      }
+    }
+  },
+
+  {
+    path: '/categories/',
+    url: "./pages/categories.html",
+    on: {
+      pageAfterIn: function(e, page){
+        var router = this;
+        var app = router.app;
+        app.request({
+          url: "https://www.themealdb.com/api/json/v1/1/categories.php", //URL de L'api
+          method: "GET", // Méthode 
+          dataType: "json", // Important, sinon vous allez récupérer un string et non un objet
+          beforeSend: function () {
+            // Avant de récupérer mes datas, j'affiche un loader 
+            //(important quand on fait un traitement pour montrer qu'il est en cours +  empêcher les impatients de cliquer partout pendant le process !)
+            app.dialog.preloader();
+          },
+          success: function (res) {
+            // res correspond à la réponse
+            console.log(res);
+            $('.categorie-id').html(res.categories[0].idCategory)
+            $('.categorie-name').html(res.meals[0].strCategory)
+            $('.categorie-thumbnail').html(res.meals[0].strCategoryThumb)
+            $('.categorie-description').html(res.meals[0].strCategoryDescription)
+           
+            // Je ferme le loader
+            app.dialog.close();
+          },
+        });
+      }
+    }
+  },
+
   {
     path: '/form/',
     url: './pages/form.html',
   },
 
-
   {
     path: '/dynamic-route/blog/:blogId/post/:postId/',
     componentUrl: './pages/dynamic-route.html',
   },
-  {
-    path: '/request-and-load/user/:userId/',
-    async: function ({ router, to, resolve }) {
-      // App instance
-      var app = router.app;
-
-      // Show Preloader
-      app.preloader.show();
-
-      // User ID from request
-      var userId = to.params.userId;
-
-      // Simulate Ajax Request
-      setTimeout(function () {
-        // We got user data from request
-        var user = {
-          firstName: 'Vladimir',
-          lastName: 'Kharlampidi',
-          about: 'Hello, i am creator of Framework7! Hope you like it!',
-          links: [
-            {
-              title: 'Framework7 Website',
-              url: 'http://framework7.io',
-            },
-            {
-              title: 'Framework7 Forum',
-              url: 'http://forum.framework7.io',
-            },
-          ]
-        };
-        // Hide Preloader
-        app.preloader.hide();
-
-        // Resolve route to load page
-        resolve(
-          {
-            componentUrl: './pages/request-and-load.html',
-          },
-          {
-            props: {
-              user: user,
-            }
-          }
-        );
-      }, 1000);
-    },
-  },
+  
   // Default route (404 page). MUST BE THE LAST
   {
     path: '(.*)',
