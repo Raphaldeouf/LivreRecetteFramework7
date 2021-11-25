6600bb79a3f51958803d30b7854681242be7346c
Raphaldeouf <raphaelvannier2001@gmail.com>
2021-11-25 10:47:53 +0100

    Ajout catalog + categories fonctionnant

--- www/js/routes.js
+++ www/js/routes.js
@@ -4,10 +4,6 @@ var routes = [
     path: '/',
     url: '/index.html'
   },
-  {
-    path: '/about/',
-    url: './pages/about.html',
-  },
 
   {
     path: '/home/',
@@ -33,10 +29,10 @@ var routes = [
           },
           success: function (res) {
             // res correspond à la réponse
-            console.log(res);
+            //console.log(res);
             $('.recipe-name').html(res.meals[0].strMeal)
-            $('.recipe-thumbnail').html(res.meals[0].strMealThumb)
             $('.recipe-instructions').html(res.meals[0].strInstructions)
+            $('.recette-thumbnail').attr('src', res.meals[0].strMealThumb );
 
             // Je ferme le loader
             app.dialog.close();
@@ -47,8 +43,57 @@ var routes = [
   },
 
   {
-    path: '/categories/',
+    path: '/categories/:categorieId/',
     url: "./pages/categories.html",
+    on: {
+      pageAfterIn: function(e, page){
+        var router = this;
+        var app = router.app;
+        var categorieId = page.route.params.categorieId;
+        app.request({
+          url: "https://www.themealdb.com/api/json/v1/1/filter.php?c=" + categorieId, //URL de L'api
+          method: "GET", // Méthode 
+          dataType: "json", // Important, sinon vous allez récupérer un string et non un objet
+          beforeSend: function () {
+            // Avant de récupérer mes datas, j'affiche un loader 
+            //(important quand on fait un traitement pour montrer qu'il est en cours +  empêcher les impatients de cliquer partout pendant le process !)
+            app.dialog.preloader();
+          },
+          success: function (res) {
+            // res correspond à la réponse
+            //console.log(res.meals);
+            res.meals.forEach(meal => {
+              //console.log(meal);
+              $('.categorie-name').html(categorieId);
+
+              $('.categorie-list').append(`<li>
+              <a href="/recette/${meal.idMeal}/" class="item-link item-content recette-link">
+                <div class="item-media"><img src="${meal.strMealThumb}" width="100" /></div>
+                <div class="item-inner">
+                  <div class="item-title-row">
+                    <div class="item-title"> ${meal.strMeal} </div>
+                  </div>
+                </div>
+              </a>
+            </li>`)
+              
+            });
+            // Je ferme le loader
+            app.dialog.close();
+          },
+        });
+      }
+    }
+  },
+
+  {
+    path: '/settings/',
+    url: './pages/settings.html',
+  },
+
+  {
+    path: '/catalog/',
+    url: "./pages/catalog.html",
     on: {
       pageAfterIn: function(e, page){
         var router = this;
@@ -64,28 +109,30 @@ var routes = [
           },
           success: function (res) {
             // res correspond à la réponse
-            console.log(res);
-            $('.categorie-id').html(res.categories[0].idCategory)
-            $('.categorie-name').html(res.meals[0].strCategory)
-            $('.categorie-thumbnail').html(res.meals[0].strCategoryThumb)
-            $('.categorie-description').html(res.meals[0].strCategoryDescription)
-           
+            //console.log(res.categories);
+            res.categories.forEach(categorie => {
+              //console.log(categorie);
+                 
+              $('.catalog-list').append(`<li>
+              <a href="/categories/${categorie.strCategory}/" class="item-link item-content">
+                <div class="item-media"><img src="${categorie.strCategoryThumb}" width="100" /></div>
+                <div class="item-inner">
+                  <div class="item-title-row">
+                    <div class="item-title">${categorie.strCategory}</div>
+                  </div>
+                  <div class="item-text"> ${categorie.strCategoryDescription} </div>
+                </div>
+              </a>
+            </li> `)
+
+              
+            });
             // Je ferme le loader
             app.dialog.close();
           },
         });
       }
     }
-  },
-
-  {
-    path: '/form/',
-    url: './pages/form.html',
-  },
-
-  {
-    path: '/dynamic-route/blog/:blogId/post/:postId/',
-    componentUrl: './pages/dynamic-route.html',
   },
   
   // Default route (404 page). MUST BE THE LAST
